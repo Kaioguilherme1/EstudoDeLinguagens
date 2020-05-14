fc7cd7b66c2426689b15a92052025dc0a7faf675
kaio Guilherme <kaiogiulherme111@gmail.com>
2020-05-14 18:21:41 -0400

    1

---
+++ Desafios/DE-001.py
@@ -0,0 +1,3 @@
+#Desafio-001:crie um programa que escreva "/óla, mundo"na tela
+#=============================================================
+print('Hello, Word!!!')
---
+++ Desafios/DE-003.py
@@ -0,0 +1,6 @@
+#crie um programa que leia dois numeros e de a soma deles.
+#=========================================================
+n1 = float(input('digite um numero >>>'))
+n2 = float(input('digite outro numero >>>'))
+soma = n1 + n2
+print('{} + {} = {}'.format(n1, n2 ,soma))
