2d454543b450fa6516e638d1a2042f5d16407364
kaio Guilherme <kaiogiulherme111@gmail.com>
2020-05-14 19:03:46 -0400

    DE 1-7
    atualização dos desafios feitos dia 14/06/2020 cuja os numero do titulo sao
    os desafios feitos

---
+++ Desafios/DE-006.py
@@ -0,0 +1,6 @@
+#
+#
+n = float(input('Digite um valor >>>'))
+print("o dobro de {} é {}.".format(n, 2*n))
+print("O triplo de {} é {}.".format(n, 3*n))
+print("{}² = {}.".format(n, n**2))
---
+++ Desafios/DE-007.py
@@ -0,0 +1,6 @@
+#Desenvolva um programa que leia as duas notas de um aluno,
+#calcule e mostre a sua média.
+
+n1 = int(input("Digite sua primeira nota >>>"))
+n2 = int(input('digite sua segundo nota >>>'))
+print("sua media é {}.".format((n1+n2)/2))
