f34776fa8867910c4953da196cc8d59bdad807f5
kaio Guilherme <kaioguilherme444@gmail.com>
2022-07-13 11:26:16 -0400

    Update hash.py

--- Estrutura de dados/TabelaHash/hash.py
+++ Estrutura de dados/TabelaHash/hash.py
@@ -1,48 +1,96 @@
-
-from posixpath import split
-import re
-
-
+#Author: Kaio Guilherme
 class hashtable:
     def __init__(self, maxsize):
-        self.list = []
+        self.list = [[] for _ in range(maxsize)]
         self.maxsize = maxsize
         self.size = 0
-        for i in range(maxsize):
-            self.list.append(None)
+        
 
     def __hashFunc(self, key):
         soma = 0
-        if key is str
         for i in range(len(key)-1):
             soma = soma + ord(key[i])
-        return soma % 7    
+        return soma % self.maxsize  
     
-    def __LoadFactor(self):
-        return self.size/self.maxsize
-
     def getTable(self):
         return self.list
 
     def insert(self, key, value):
         if self.size < self.maxsize:
-            k = self.__hashFunc(key)
-            print(k)
-            if self.list[k] is None:
-                self.list[k] = value
-                self.size += 1
+            hash_key = self.__hashFunc(key)
+            key_exists = False
+            bucket = self.list[hash_key]
+            for i, kv in enumerate(bucket):
+                k, v = kv
+                if key == k:
+                    key_exists = True
+                break
+
+            if key_exists:
+                bucket[i] = ((key, value))
             else:
-                
-                print('local ocupado')
+                bucket.append((key, value))
+        else:
+            print("Hash Full")
+
+    def delete(self, key):
+        hash_key = self.__hashFunc(key)
+        key_exists = False
+        bucket = self.list[hash_key]
+        for i, kv in enumerate(bucket):
+            k, v = kv
+            if key == k:
+                key_exists = True
+                break
+        if key_exists:
+            del bucket[i]
+            print('Key {} deleted'.format(key))
+        else:
+            print('Key {} not found'.format(key))
+
     def get(self, key):
-        return self.list[self.__hashFunc(key)]
+        hash_key = self.__hashFunc(key)
+        key_exists = False
+        bucket = self.list[hash_key]
+        for i, kv in enumerate(bucket):
+            k, v = kv
+            if key == k:
+                key_exists = True
+                break
+        if key_exists:
+            return bucket[i]
+        else:
+            print('Key {} not found'.format(key))
+    
+    def printHashTable(self):
+        print("|========================|")
+        print("|       Hash table       |")
+        print("|========================|")
+        for bucket in self.list:
+                for i in bucket:
+                    print("| {:<12} |   {:<4}  |".format(i[0], i[1]))
+        print("|========================|")
+        print("| max size: {:<2} | size: {:<2}|".format(self.maxsize, self.size))
+        print("|========================|")
+
+
 
 
 
 tabela = hashtable(10)
 
-tabela.insert("ana", 35485756)
-tabela.insert("Lisa", 84527856)
-tabela.insert("Carlos", 45876524)
-print(tabela.get("carro"))
+tabela.insert("ana", 9.2)
+tabela.insert("Lisa", 2.5)
+tabela.insert("Carlos", 6.4)
+tabela.insert("maria", 7.9)
+tabela.insert("Jordan", 8.5)
+tabela.insert("John", 9.5)
+tabela.insert("jorge", 8.5)
 
+
+print(tabela.getTable())
+print("Buscar key ana")
+print(tabela.get("ana"))
+tabela.printHashTable()
+tabela.delete("Jordan")
+tabela.printHashTable()
