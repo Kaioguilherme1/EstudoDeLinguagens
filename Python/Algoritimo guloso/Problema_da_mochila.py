124656373f6788c098b571c85df0e861fb7e5129
kaio Guilherme <kaioguilherme444@gmail.com>
2022-07-15 19:57:50 -0400

    Update Problema_da_mochila.py

--- Python/Algoritimo guloso/Problema_da_mochila.py
+++ Python/Algoritimo guloso/Problema_da_mochila.py
@@ -1,21 +1,24 @@
+#Author Kaio Guilherme
+#Co-Author Lucas Anderson
 
 class item:
-    def __init__(self,nome ,valor : int, peso : int):
+    def __init__(self, nome, valor: int, peso: int):
         self.nome = nome
         self.valor = valor
         self.peso = peso
-    
+
     def get_valor(self):
         return self.valor
-    
+
     def get_peso(self):
         return self.peso
-    
+
     def get_nome(self):
         return self.nome
 
+
 class mochila:
-    def __init__(self, peso_maximo=16):
+    def __init__(self, peso_maximo=24):
         self.peso_maximo = peso_maximo
         self.peso = 0
         self.items = []
@@ -30,23 +33,25 @@ class mochila:
         else:
             print("Muito pesado para adicionar na mochila")
             return False
-    
+
     def get_valor_total(self):
         return self.valor_total
-    
+
     def get_peso(self):
         return self.peso
-    
+
     def get_peso_maximo(self):
-        return self.peso_maximo 
-    
+        return self.peso_maximo
+
     def get_items(self):
         return self.items
+
 
 def delete_item(item, items):
     for i in range(len(items)-1):
         if items[i] == item:
             items.pop(i)
+
 
 def print_inventario(nome, items):
     valor_total = 0
@@ -55,12 +60,13 @@ def print_inventario(nome, items):
     print("|                 {:<10}              |".format(nome))
     print("|=========================================|")
     for item in items:
-        print("| {:<16} |   {:<2} kg  | R${:<4}    |".format(item.get_nome(), item.get_peso(), item.get_valor()))
+        print("| {:<16} |   {:<2} kg  |   {:<4}    |".format(item.get_nome(), item.get_peso(), item.get_valor()))
         valor_total += int(item.get_valor())
         peso_total += int(item.get_peso())
     print("|=========================================|")
-    print("| peso total:{:<2} kg |  total: R${:<6}     |".format(peso_total, valor_total))
+    print("| peso total: {:<2}kg                        |".format(peso_total, valor_total))
     print("|=========================================|")
+
 
 def metodo_guloso(items, mochila):
     item_mais_caro = items[0]
@@ -69,11 +75,11 @@ def metodo_guloso(items, mochila):
             disponivel = mochila.peso_maximo - mochila.peso
             if item.peso <= disponivel:
                 item_mais_caro = item
-    
-    pegou = mochila.adicionar_Item(item_mais_caro)  
-    delete_item(item_mais_caro, items)  
+
+    pegou = mochila.adicionar_Item(item_mais_caro)
+    delete_item(item_mais_caro, items)
     print("\n**************************************************************************\n")
-    print_inventario("loja", items)
+    print_inventario("itens", items)
     print("++++++++++++++++++++++++++++++++++++++++++++")
     print_inventario("Mochila", mochila.get_items())
     print("\n**************************************************************************\n")
@@ -81,15 +87,15 @@ def metodo_guloso(items, mochila):
         metodo_guloso(items, mochila)
 
 
-item1 = item("banana",5,1)
-item2 = item("dvd",900,5)
-item3 = item("peso",500,10)
-item4 = item("barra_de_ouro",5000, 10)
-item5 = item("celular",2000, 1)
-item6 = item("air dots",1000, 1)
-item7 = item("cordao de ouro",4000, 6)
-item8 = item("sansung_x",3000, 1)
+comida = item("comida", 17, 5)
+agua = item("agua", 20, 2)
+tv = item("tv", 1, 7)
+kit = item("kit socorro", 20, 4)
+barraca = item("barraca", 15, 10)
+repelente = item("repelente", 2, 1)
+geladeira = item("geladeira", 1, 22)
 
-items = [item1, item2, item3, item4, item5, item6, item7, item8]
+items = [comida, tv, agua, repelente, kit, barraca, geladeira]
+
 mochila = mochila()
 metodo_guloso(items, mochila)
